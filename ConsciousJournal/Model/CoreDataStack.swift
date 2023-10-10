2266072ac487d536805de13e8b9e7a423c5da676
dnstrach <dnstrach@gmail.com>
2023-10-10 11:21:48 -0700

    frc, date sections, dark/light mode

---
+++ ConsciousJournal/Model/CoreDataStack.swift
@@ -0,0 +1,8 @@
+//
+//  CoreDataStack.swift
+//  ConsciousJournal
+//
+//  Created by Dominique Strachan on 10/10/23.
+//
+
+import Foundation
---
+++ ConsciousJournal/Model/Journal+Initializer.swift
@@ -0,0 +1,8 @@
+//
+//  Journal+Initializer.swift
+//  ConsciousJournal
+//
+//  Created by Dominique Strachan on 10/10/23.
+//
+
+import Foundation
