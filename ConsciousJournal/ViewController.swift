e5aaa173c010cd4ae43803d277cb55017f4d12d0
dnstrach <dnstrach@gmail.com>
2023-10-10 10:26:50 -0700

    Initial Commit

---
+++ ConsciousJournal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ConsciousJournal
+//
+//  Created by Dominique Strachan on 10/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
